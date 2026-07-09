7425f73fc4f8c25dffc607555d37b1fed3b09284
João Pedro Inácio <j.p.inacio2004@gmail.com>
2026-07-09 13:18:56 +0100

    Adicionar rotas frontend

--- backend/backend_stand/config/urls.py
+++ backend/backend_stand/config/urls.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, "index.html")
+
+
+def veiculos(request):
+    return render(request, "veiculos.html")
+
+
+def sobre(request):
+    return render(request, "SobreNos.html")
+
+
+def contacto(request):
+    return render(request, "contacto.html")
+
 
 urlpatterns = [
+    path("", home, name="home"),
+    path("veiculos/", veiculos, name="veiculos"),
+    path("sobre/", sobre, name="sobre"),
+    path("contacto/", contacto, name="contacto"),
+
     path("admin/", admin.site.urls),
     path("api/", include("stand.urls")),
 ]
